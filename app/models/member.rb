39dc11183e830d915d5edd3703db4b6876f78f00
Guilherme Rutemberg <guilhermerutemfelix@gmail.com>
2024-06-06 18:49:51 -0300

    Configuração das validações da model Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+    has_many :tasks, dependent: :destroy
+  
+    validates :name, presence: true, length: { minimum: 5 }
+    validates :email, presence: true, uniqueness: true
 end
