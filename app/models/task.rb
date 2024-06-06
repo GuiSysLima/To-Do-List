2604457ce5ef498e8aacfde6a4b95b9c7461f45b
Guilherme Rutemberg <guilhermerutemfelix@gmail.com>
2024-06-06 18:50:33 -0300

    Configuração da validação da model Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :member
+    
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :description, length: { maximum: 140 }
+  validates :finished, inclusion: { in: [true, false] }
+  validates :priority, presence: true, inclusion: { in: %w[Baixa Média Alta] }
+  
+  before_validation :set_default_priority, on: :create
+
+  private
+  
+  def set_default_priority
+    self.priority ||= 'Baixa'
+  end
 end
